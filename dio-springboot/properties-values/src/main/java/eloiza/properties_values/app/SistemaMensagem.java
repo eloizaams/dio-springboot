e4170a7b448f2507a0288dbf847c723c88b3e07b
eloizaams <eloizaams@yahoo.com.br>
2024-08-22 08:32:37 -0300

    configuration properties

--- dio-springboot/properties-values/src/main/java/eloiza/properties_values/app/SistemaMensagem.java
+++ dio-springboot/properties-values/src/main/java/eloiza/properties_values/app/SistemaMensagem.java
@@ -1,28 +1,20 @@
 package eloiza.properties_values.app;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SistemaMensagem implements CommandLineRunner{
-	//valores declarados no arquivo application.properties
-	//uma vez que "name" não for encontrado no arquivo ele coloca NoReplay no lugar
-	@Value("${name:NoReplay}")
-	private String nome;
 	
-	@Value("${email}")
-	private String email;
+	@Autowired
+	private Remetente remetente;
 	
-	@Value("${telefones}")
-	private List<Long> telefones;
-
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("Mensagem enviada por: " + nome +"\nE-mail: " + email + 
-							"\nCom telefones para contato: " + telefones);
+		System.out.println("Mensagem enviada por: " + remetente.getNome() +
+							"\nE-mail: " + remetente.getEmail() + 
+							"\nCom telefones para contato: " + remetente.getTelefones());
 		System.out.println("Seu cadastro foi aprovado");
 	}
 
