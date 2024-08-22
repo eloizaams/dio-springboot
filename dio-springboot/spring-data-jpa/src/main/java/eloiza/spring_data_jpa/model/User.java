cc7d55391ac71f80fd5124f6bb199e2b3a597288
eloizaams <eloizaams@yahoo.com.br>
2024-08-22 12:38:36 -0300

    feat: Conexao com Postgres

--- dio-springboot/spring-data-jpa/src/main/java/eloiza/spring_data_jpa/model/User.java
+++ dio-springboot/spring-data-jpa/src/main/java/eloiza/spring_data_jpa/model/User.java
@@ -5,13 +5,15 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
-@Entity(name="users")
+@Entity
+@Table(name = "tab_user")
 public class User {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name="users_id")
+	@Column(name="user_id")
 	private Integer id;
 	
 	@Column(length=50, nullable=false)
